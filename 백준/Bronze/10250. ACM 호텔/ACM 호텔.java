2c807a0b8697cc3f9ef421c159fa6602320a91a8
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-03-31 12:19:00 +0900

    [Bronze III] Title: ACM 호텔, Time: 172 ms, Memory: 14796 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,53 +1,75 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//
+//    public static int parse(String a) {
+//        return Integer.parseInt(a);
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st;
+//
+//        int t = parse(br.readLine());
+//
+//        int[] a;
+//
+//        for(int i = 0; i < t; i++) {
+//            st = new StringTokenizer(br.readLine());
+//
+//            int h = parse(st.nextToken());
+//            int w = parse(st.nextToken());
+//            int n = parse(st.nextToken());
+//
+//            a = new int[h*w];
+//            int count = 0;
+//
+//            for(int k = 1; k <= w; k++) {
+//                for(int l = 1; l <= h; l++) {
+//                    if(k < 10) {
+//                        sb.append(l).append(0).append(k);
+//                    } else {
+//                        sb.append(l).append(k);
+//                    }
+//                    a[count] = Integer.parseInt(sb.toString());
+//                    count++;
+//                    sb.setLength(0);
+//                }
+//            }
+//
+//            bw.write(String.valueOf(a[n-1]));
+//            bw.write("\n");
+//        }
+//
+//        bw.close();
+//    }
+//}
+
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static int parse(String a) {
-        return Integer.parseInt(a);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = parse(br.readLine());
+        int tcc = Integer.parseInt(br.readLine());
 
-        int[] a;
-
-        for(int i = 0; i < t; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int h = parse(st.nextToken());
-            int w = parse(st.nextToken());
-            int n = parse(st.nextToken());
-
-            a = new int[h*w];
-            int count = 0;
-
-            for(int k = 1; k <= w; k++) {
-                for(int l = 1; l <= h; l++) {
-                    if(k < 10) {
-                        sb.append(l).append(0).append(k);
-                    } else {
-                        sb.append(l).append(k);
-                    }
-                    a[count] = Integer.parseInt(sb.toString());
-                    count++;
-                    sb.setLength(0);
-                }
-            }
-
-            bw.write(String.valueOf(a[n-1]));
-            bw.write("\n");
+        for (int i = 0; i < tcc; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            boolean top = n % y == 0;
+            System.out.printf("%d%02d\n", top ? y : (n % y), top ? (n / y) : (n / y + 1));
         }
-
-        bw.close();
+        //       bw.flush();
     }
 }
