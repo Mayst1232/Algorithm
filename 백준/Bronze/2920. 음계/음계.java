53f8391df9053c0b804315cfb5c6f9a4ecce1641
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-04-01 16:13:06 +0900

    [Bronze II] Title: 음계, Time: 128 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int parse(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int[] a = new int[8];
+        String flag = "";
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < 8; i ++) {
+            a[i] = parse(st.nextToken());
+        }
+
+        for(int i = 0; i < 7; i++) {
+            if(flag.equals("")) {
+                if(a[i] == a[i+1] - 1) {
+                    flag = "ascending";
+                    continue;
+                } else if (a[i] == a[i + 1] + 1) {
+                    flag = "descending";
+                    continue;
+                }
+            } else if(flag.equals("ascending")) {
+                if(a[i] == a[i+1] - 1) {
+                    continue;
+                }
+            } else if (flag.equals("descending")) {
+                if(a[i] == a[i+1] + 1) {
+                    continue;
+                }
+            }
+
+            flag = "mixed";
+            break;
+        }
+
+        System.out.println(flag);
+    }
+}
