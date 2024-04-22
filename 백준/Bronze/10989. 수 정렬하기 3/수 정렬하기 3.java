fdd78ecf9177411271777d441d43e3a52f2eef0b
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-04-22 22:43:45 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1664 ms, Memory: 336704 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static int parse(String s) {
+      return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = parse(br.readLine());
+        int[] count = new int[10001];
+
+        for(int i = 0; i < n; i++) {
+            count[parse(br.readLine())]++;
+        }
+
+        for(int i = 1; i < 10001; i++) {
+            while(count[i] > 0) {
+                sb.append(i).append("\n");
+                count[i]--;
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
