6f75c6a1a3591becc4c1d3d0437d37130ed19d79
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-05-26 08:06:28 +0900

    [Bronze IV] Title: 입실 관리, Time: 120 ms, Memory: 14148 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            String s = br.readLine();
+            sb.append(s.toLowerCase()).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
