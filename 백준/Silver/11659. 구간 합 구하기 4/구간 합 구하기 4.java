1c63da29821967bc347cca8f84fc507d34b6e632
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-06-07 11:35:12 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 568 ms, Memory: 63640 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
@@ -20,25 +18,40 @@ public class Main {
         int n = parse(st.nextToken());
         int m = parse(st.nextToken());
 
-        int[] a = new int[n + 1];
-        int[] total = new int[n + 1];
+        int[] a = new int[n];
 
         st = new StringTokenizer(br.readLine());
 
-        for(int i = 1; i < a.length; i++) {
+        for(int i = 0; i < a.length; i++){
             a[i] = parse(st.nextToken());
-            total[i] = total[i-1] + a[i];
         }
 
-        while (m-- > 0) {
+        int[] s = new int[n];
+        s[0] = a[0];
+
+        for(int i = 1; i < s.length; i++) {
+            s[i] = s[i-1] + a[i];
+        }
+
+        for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
+            int k = parse(st.nextToken()) - 1;
+            int j = parse(st.nextToken()) - 1;
 
-            int x = parse(st.nextToken());
-            int y = parse(st.nextToken());
-            bw.write(String.valueOf(total[y] - total[x - 1]));
-            bw.write("\n");
+            if(k == j) {
+                sb.append(a[k]).append("\n");
+                continue;
+            }
+
+            if(k == 0) {
+                sb.append(s[j]).append("\n");
+                continue;
+            }
+
+            sb.append(s[j] - s[k-1]).append("\n");
         }
 
-        bw.close();
+        System.out.print(sb);
     }
+
 }
