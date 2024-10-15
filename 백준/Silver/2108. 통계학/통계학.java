368b3056e0dd14f60c94e00f2beb0bde70a995c1
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-10-15 17:15:43 +0900

    [Silver III] Title: 통계학, Time: 284 ms, Memory: 41668 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+public class Main {
+    public static int parse(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = parse(br.readLine());
+
+        int[] arr = new int[8001];
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int median = 10000;
+        int manyTimes = 10000;
+
+        for(int i = 0; i < n; i++) {
+            int a = parse(br.readLine());
+            sum += a;
+            arr[a + 4000]++;
+
+            if(max < a) {
+                max = a;
+            }
+            if(min > a) {
+                min = a;
+            }
+        }
+
+        int count = 0;
+        int maxManyTimes = 0;
+        boolean flag = false;
+
+        for(int i = min + 4000; i <= max + 4000; i++) {
+            if(arr[i] > 0) {
+                if(count < (n + 1) / 2) {
+                    count += arr[i];
+                    median = i - 4000;
+                }
+
+                if(maxManyTimes < arr[i]) {
+                    maxManyTimes = arr[i];
+                    manyTimes = i - 4000;
+                    flag = true;
+                } else if(maxManyTimes == arr[i] && flag == true) {
+                    manyTimes = i - 4000;
+                    flag = false;
+                }
+            }
+        }
+
+        System.out.println((int)Math.round((double)sum / n));
+        System.out.println(median);
+        System.out.println(manyTimes);
+        System.out.println(max - min);
+    }
+}
