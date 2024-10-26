7ba88857c9b672a2c86f798c7b12a53a2ed53c59
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-10-26 23:57:27 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 168 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.next();
+        
+        if(s.equals("NLCS")) {
+            System.out.println("North London Collegiate School");
+        } else if(s.equals("BHA")) {
+            System.out.println("Branksome Hall Asia");
+        } else if(s.equals("KIS")) {
+            System.out.println("Korea International School");
+        } else if(s.equals("SJA")) {
+            System.out.println("St. Johnsbury Academy");
+        }
+    }
+}
