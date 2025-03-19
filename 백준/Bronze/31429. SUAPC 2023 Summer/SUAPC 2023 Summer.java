7387f7a68ac56b29594751f6d3534c01d3541ad1
Mayst <48579987+Mayst1232@users.noreply.github.com>
2025-03-19 20:42:18 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        
+        if(a == 1) {
+            System.out.println("12 1600");
+        } else if (a == 2) {
+            System.out.println("11 894");
+        } else if (a == 3) {
+            System.out.println("11 1327");
+        } else if (a == 4) {
+            System.out.println("10 1311");
+        } else if (a == 5) {
+            System.out.println("9 1004");
+        } else if (a == 6) {
+            System.out.println("9 1178");
+        } else if (a == 7) {
+            System.out.println("9 1357");
+        } else if (a == 8) {
+            System.out.println("8 837");
+        } else if (a == 9) {
+            System.out.println("7 1055");
+        } else if (a == 10) {
+            System.out.println("6 556");
+        } else if (a == 11) {
+            System.out.println("6 773");
+        }
+    }
+}
